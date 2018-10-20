41264a1237e0d25ac6c0d2d45f400234980a4fb1
Jason Trobman <mdchoudhury@digitalpulp.com>
2018-10-19 21:00:29 -0400

    Initial Commit

---
+++ Tipster/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tipster
+//
+//  Created by MD Abir Choudhury on 10/19/18.
+//  Copyright © 2018 MD Abir Choudhury. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ TipsterTests/TipsterTests.swift
@@ -0,0 +1,34 @@
+//
+//  TipsterTests.swift
+//  TipsterTests
+//
+//  Created by MD Abir Choudhury on 10/19/18.
+//  Copyright © 2018 MD Abir Choudhury. All rights reserved.
+//
+
+import XCTest
+@testable import Tipster
+
+class TipsterTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
